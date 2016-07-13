a6167b775a0d24c56bb6ceaef00cc29641b15658
ch <dodecalogue@gmail.com>
2016-07-13 11:54:31 -0500

    Move content to Rmd

--- summaries/bigMessyEarlyTopics.R
+++ summaries/bigMessyEarlyTopics.R
@@ -49,60 +49,6 @@ print(frame1)
 
 
 
-y <- list(sequ = 1:4, ok = 0.4, wonderful = c(1,5,4), a=1, b=2)
-class(y[1])
-class(y[[1]])
-y$ok
-y["ok"]
-y[["ok"]]
-## LOOK MORE AT THE SPECIFICS OF [] and [[]]
-
-y[c(1,3)]
-
-# [[ ]] can be used for computed indices, whereas $ can't
-name <- "sequ"
-y[[name]]   # y$name errors
-
-# [[ ]] can extract nested elems
-y[[c(1,2)]]   # equivalent to y[[1]][[2]]
-
-# matrix indexing
-mat <- matrix(1:6, 2,3)  # a 2x3 with 1 through 6, remember it fills cols from UL
-mat[1,2]   # gives 3
-mat[1,]    # first row, 1 3 5
-mat[,1]    # first col, 1 2
-
-# Recall [] returns an object of the same class however:
-# By default, retrieving a single elem of a matrix returns a vector of length 1, rather than a 1x1 matrix
-# And similarly retrieving a row/col returns a vector
-# This can be changed by setting drop = FALSE
-mat[1,2, drop = FALSE]   # 1x1 matrix
-mat[1, , drop = FALSE]   # 1x3 matrix
-
-
-# Partial Matching
-# allowed with $ and [[ ]], only really helpful on the commandline
-y$wo  											# matches the name wonderful
-y[["wo"]]  									# NULL
-y[["wo", exact = FALSE]]		# matches the name wnoderful
-
-# Helpful example of removing NA values
-x<-c(1, 3, NA, 4, 4, 3, NA)
-badx <- is.na(x)
-x[!badx]
-
-y<-c("a", "b", NA, "c", "d", "e", NA)
-good <- complete.cases(x,y)    # returns a logical vector indicating which cases have no missing values
-# Note if ie x was all NA, then good would be all FALSE
-x[good]
-y[good]
-
-data <- read.csv("someData.csv", colClasses = c("numeric", "numeric", "logical"))
-print(data)
-good <- complete.cases(data)
-print(data[good, ])
-
-
 ####### LOOP FUNCTIONS
 # https://nsaunders.wordpress.com/2010/08/20/a-brief-introduction-to-apply-in-r/
 #  Good explanations
@@ -118,35 +64,6 @@ lapply
 # }
 # <bytecode: 0x20f3bc0>
 #   <environment: namespace:base>
-
-# Remember, lists can have any kind of object as its elements
-x <- list(a = 1:5, b = rnorm(10))
-lapply(x, mean)
-# $a
-# [1] 3
-#
-# $b
-# [1] -0.0474927
-
-# What goes in will try to be coerced as a list
-# It will return a list
-
-x <- 1:4
-lapply(x, runif)  # runif(n) returns n random uniform
-# [[1]]
-# [1] 0.7108169   # generating 1 random uniforms
-#
-# [[2]]
-# [1] 0.710346 0.474639   # 2 random uniforms
-#
-# [[3]]
-# [1] 0.9688282 0.5138259 0.9005831
-#
-# [[4]]
-# [1] 0.59789127 0.96521355 0.25745983 0.08854886
-
-# Passing arguments to the runif function
-lapply(x, runif, min = 0, max = 10)
 
 x <- list(a = matrix(1:4, 2,2), b= matrix(1:6, 3,2))
 # suppose we want the first col of each
